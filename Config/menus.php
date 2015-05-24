eb0ac37b5260bae2c4f439755b029b9fd9cde530
xLink <xLink@cybershade.org>
2015-05-24 22:17:43 +0100

    added social menu item to user edit panel

--- Config/menus.php
+++ Config/menus.php
@@ -15,4 +15,14 @@ return [
         ]
     ],
 
+    'backend_user_menu' => [
+        [
+            'route'      => ['admin.user.provider', ['auth_user_id' => app('request')->segment(3)]],
+            'text'       => 'Social Providers',
+            'icon'       => 'fa-share-alt-square',
+            'order'      => 3,
+            'permission' => 'manage.update@auth_user'
+        ],
+    ],
+
 ];
--- Http/routes-backend.php
+++ Http/routes-backend.php
@@ -1,12 +1,35 @@
 <?php
 
+use Illuminate\Routing\Router;
+
 
 // URI: /{backend}/social
 $router->group([
     'prefix'        => 'config/social',
     'middleware'    => 'hasPermission',
     'hasPermission' => 'manage@social_admin'
-], function ($router) {
+], function (Router $router) {
 
     $router->get('/', ['as' => 'admin.config.social', 'uses' => 'SocialManagerController@getIndex']);
+});
+
+
+// URI: /{backend}/users
+$router->group([
+    'prefix'        => 'users',
+    'middleware'    => 'hasPermission',
+    'hasPermission' => 'manage@auth_user'
+], function (Router $router) {
+
+    $router->group(['prefix' => '{auth_user_id}', 'namespace' => 'User'], function (Router $router) {
+
+        $router->group(['middleware' => 'hasPermission', 'hasPermission' => 'manage.update@auth_user'], function (Router $router) {
+
+            $router->group(['prefix' => 'providers'], function (Router $router) {
+                $router->post('/', ['uses' => 'ProvidersController@postForm']);
+                $router->get('/', ['as' => 'admin.user.provider', 'uses' => 'ProvidersController@getForm']);
+            });
+
+        });
+    });
 });
