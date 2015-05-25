95721625da516fa8851d6b10175c3b101d70ba62
xLink <xLink@cybershade.org>
2015-05-25 02:36:32 +0100

    updated menus

--- Config/menus.php
+++ Config/menus.php
@@ -17,7 +17,7 @@ return [
 
     'backend_user_menu' => [
         [
-            'route'      => ['admin.user.provider', ['auth_user_id' => app('request')->segment(3)]],
+            'route'      => ['admin.user.provider', ['auth_user_id' => 'segment:3']],
             'text'       => 'Social Providers',
             'icon'       => 'fa-share-alt-square',
             'order'      => 3,
