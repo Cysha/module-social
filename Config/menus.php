fc01573cd4e23f44a5ee1c8d01ab21b4a469abdf
xLink <xLink@cybershade.org>
2015-06-23 14:20:44 +0100

    updated menu to support the new ordering

--- Config/menus.php
+++ Config/menus.php
@@ -5,23 +5,27 @@ return [
     'backend_sidebar' => [
 
         'System' => [
-            [
-                'route'      => 'admin.config.social',
-                'text'       => 'Social Manager',
-                'icon'       => 'fa-share-alt-square',
-                'order'      => 1,
-                'permission' => 'manage@social_admin'
+            'children' => [
+                [
+                    'route'      => 'admin.config.social',
+                    'text'       => 'Social Manager',
+                    'icon'       => 'fa-share-alt-square',
+                    'order'      => 1,
+                    'permission' => 'manage@social_admin'
+                ],
             ],
         ]
     ],
 
     'backend_user_menu' => [
-        [
-            'route'      => ['admin.user.provider', ['auth_user_id' => 'segment:3']],
-            'text'       => 'Social Providers',
-            'icon'       => 'fa-share-alt-square',
-            'order'      => 3,
-            'permission' => 'manage.update@auth_user'
+        'children' => [
+            [
+                'route'      => ['admin.user.provider', ['auth_user_id' => 'segment:3']],
+                'text'       => 'Social Providers',
+                'icon'       => 'fa-share-alt-square',
+                'order'      => 3,
+                'permission' => 'manage.update@auth_user'
+            ],
         ],
     ],
 
